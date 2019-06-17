e378d38270347d4ab560f25c56f5c4c77a0d4a64
wioxio <mingyous@usc.edu>
2019-06-17 16:19:54 -0700

    Update simple_NN.py

--- simple_NN.py
+++ simple_NN.py
@@ -49,30 +49,36 @@ X = tf.placeholder(dtype=tf.float32, shape=[None, input_dim])
 Y = tf.placeholder(dtype=tf.float32, shape=[None])
 
 
-# Layer 1: Variables for hidden weights and biases
-W_hidden_1=tf.get_variable("W1", shape=[input_dim, n_neurons_1],
-                         initializer=tf.contrib.layers.xavier_initializer())
-#This allows us to pass in a custom initializer for our weights
-b1 = tf.Variable(tf.random_normal([n_neurons_1]))
-# Layer 2: Variables for hidden weights and biases
-W_hidden_2=tf.get_variable("W2", shape=[n_neurons_1, n_neurons_2],
-                         initializer=tf.contrib.layers.xavier_initializer())
-b2 = tf.Variable(tf.random_normal([n_neurons_2]))
+weights={
+    'W_hidden_1': tf.get_variable("W1", shape=[input_dim, n_neurons_1],initializer=tf.contrib.layers.xavier_initializer()),
+    'W_hidden_2': tf.get_variable("W2", shape=[n_neurons_1, n_neurons_2],initializer=tf.contrib.layers.xavier_initializer()),
+    'W_out': tf.get_variable("W3", shape=[n_neurons_2, n_target],initializer=tf.contrib.layers.xavier_initializer())
+}
 
-# Output layer: Variables for output weights and biases
-W_out = tf.get_variable("W3", shape=[n_neurons_2, n_target],
-                         initializer=tf.contrib.layers.xavier_initializer())
-b3 = tf.Variable(tf.random_normal([n_target]))
+biases={
 
-# Hidden layer
-hidden_1 = tf.nn.relu(tf.add(tf.matmul(X, W_hidden_1 ), b1))
-hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1 , W_hidden_2), b2))
+'b1' : tf.Variable(tf.random_normal([n_neurons_1])),
 
+'b2' : tf.Variable(tf.random_normal([n_neurons_2])),
+
+'b3' : tf.Variable(tf.random_normal([n_target]))
+
+}
+
+def run_NN(input_x, W, B):
+    # Hidden layer
+    hidden_1 = tf.nn.relu(tf.add(tf.matmul(input_x, W['W_hidden_1'] ), B['b1']))
+    hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1 , W['W_hidden_2']), B['b2']))
 # Output layer (must be transposed)
-out_pre = tf.transpose(tf.add(tf.matmul(hidden_2, W_out), b3))
-out=tf.tanh(out_pre)
+    out_pre = tf.transpose(tf.add(tf.matmul(hidden_2, W['W_out']), B['b3']))
+    out=tf.tanh(out_pre)
+    return out
 
-#L2
+
+
+out=run_NN(X,weights, biases)
+
+#cost function
 cost=tf.reduce_mean(tf.squared_difference(out, Y))
 
 # Optimizer
@@ -82,71 +88,72 @@ prediction=out
 equals=tf.equal(prediction, Y)
 accuracy = tf.reduce_mean(tf.cast(equals, tf.float32))
 
-# Make Session
-net = tf.Session()
-# Run initializer
-net.run(tf.global_variables_initializer())
 
 
-# Number of epochs and batch size
-epochs = 200
-batch_size = 77
+with tf.Session() as net:
+    # Run initializer
+    net.run(tf.global_variables_initializer())
+
+
+    # Number of epochs and batch size
+    epochs = #your epoch
+    batch_size = #your batch size
 
 
 
-#cv
-cv_fold=8
-cv_elCount=n//cv_fold
-#accuracy array
-accuracy_all=np.array([])
-#for cv in np.arange(cv_fold):
-cv=1
-# Training and test data
-test_start = cv * cv_elCount
-test_end = cv * cv_elCount + cv_elCount
-total_set=np.arange(n)
-train_set = np.setdiff1d(total_set,np.arange(test_start,test_end))
-#
-data_train_x = data[train_set, :]
-data_test_x = data[np.arange(test_start, test_end), :]
-data_train_y = pheno[train_set]
-data_test_y = pheno[np.arange(test_start, test_end)]
-# Build X and y
-X_train = data_train_x
-y_train = data_train_y
-X_test = data_test_x
-y_test = data_test_y
-# Setup interactive plot
-plt.ion()
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(y_test)
-line2, = ax1.plot(y_test*0.5)
-plt.show()
-avg_cost = 0
-for e in range(epochs):
-    # Minibatch training
-    for i in range(0, len(y_train) // batch_size):
-        start = i * batch_size
-        batch_x = X_train[start:start + batch_size,:]
-        batch_y = y_train[start:start + batch_size]
-        # Run optimizer with batch
-        _, c = net.run([opt,cost], feed_dict={X: batch_x, Y: batch_y})
-        print(c)
-        # Show progress
-        if np.mod(i, 10) == 0:
-            # Prediction
-            pred = net.run(prediction, feed_dict={X: X_test})
-            line2.set_ydata(pred)
-            plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
-            file_name = 'epoch_' + str(e) + '_batch_' + str(i) + '.png'
-            plt.savefig(file_name)
-            plt.pause(0.01)
-        avg_cost += c / (batch_size*epochs)
-        print("Epoch:", (e + 1),  ' Batch ' + str(i), "cost =", "{:.10f}".format(avg_cost))
+    #cv
+    cv_fold= #your fold
+    cv_elCount=n//cv_fold
+    #accuracy array
+    accuracy_all=np.array([])
+    #for cv in np.arange(cv_fold):
+    cv=1
+    # Training and test data
+    test_start = cv * cv_elCount
+    test_end = cv * cv_elCount + cv_elCount
+    total_set=np.arange(n)
+    train_set = np.setdiff1d(total_set,np.arange(test_start,test_end))
+    #
+    data_train_x = data[train_set, :]
+    data_test_x = data[np.arange(test_start, test_end), :]
+    data_train_y = pheno[train_set]
+    data_test_y = pheno[np.arange(test_start, test_end)]
+    # Build X and y
+    X_train = data_train_x
+    y_train = data_train_y
+    X_test = data_test_x
+    y_test = data_test_y
+    # Setup interactive plot
+    plt.ion()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    line1, = ax1.plot(y_test)
+    line2, = ax1.plot(y_test*0.5)
+    plt.show()
+    avg_cost = 0
+    for e in range(epochs):
+        # Minibatch training
+        for i in range(0, len(y_train) // batch_size):
+            start = i * batch_size
+            batch_x = X_train[start:start + batch_size,:]
+            batch_y = y_train[start:start + batch_size]
+            # Run optimizer with batch
+            _, c = net.run([opt,cost], feed_dict={X: batch_x, Y: batch_y})
+            print(c)
+            # Show progress
+            if np.mod(i, 10) == 0:
+                # Prediction
+                pred = net.run(prediction, feed_dict={X: X_test})
+                line2.set_ydata(pred)
+                plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
+                file_name = 'epoch_' + str(e) + '_batch_' + str(i) + '.png'
+                plt.savefig(file_name)
+                plt.pause(0.01)
+            avg_cost += c / (batch_size*epochs)
+            print("Epoch:", (e + 1),  ' Batch ' + str(i), "cost =", "{:.10f}".format(avg_cost))
 
-pred = net.run(prediction, feed_dict={X: X_test})
-# Print final MSE after Training
-mse_final = net.run(accuracy, feed_dict={X: X_test, Y: y_test})
-print(mse_final)
-accuracy_all=np.append(accuracy_all,mse_final)
+    pred = net.run(prediction, feed_dict={X: X_test})
+    # Print final MSE after Training
+    mse_final = net.run(accuracy, feed_dict={X: X_test, Y: y_test})
+    print(mse_final)
+    accuracy_all=np.append(accuracy_all,mse_final)
